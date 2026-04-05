86d3483386b00b0f587b2fa1c3b738131e58261a
Umamichi <umamichi@outlook.com>
2026-04-06 01:36:28 +0800

    feat: add katex support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 import react from '@astrojs/react';
 
@@ -13,4 +15,8 @@ export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), react()],
   adapter: cloudflare(),
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
